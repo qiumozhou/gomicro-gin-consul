c333e42856cd8641d5d04b36c40f26163e1522d5
qiumozhou <mozhouqiu@bitorobotics.ltd>
2022-01-07 14:30:45 +0800

    终于将微服务打通,美滋滋

--- server/micpro/handler/micpro.go
+++ server/micpro/handler/micpro.go
@@ -9,6 +9,8 @@ type Micpro struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Micpro) Call(ctx context.Context, req *micpro.Request, rsp *micpro.Response) error {
+
+	//此处编写需要处理的业务逻辑，
 	rsp.Msg = "wqw"
 	return nil
 }
--- web/main.go
+++ web/main.go
@@ -6,22 +6,26 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro"
-	//"github.com/micro/go-micro/client"
 	"github.com/micro/go-plugins/registry/consul"
-	//"time"
+	"net/http"
 )
 
 func microCall(context *gin.Context){
-
+	//创建consul连接
 	reg := consul.NewRegistry()
+	//将consul连接接入到微服务
 	service := micro.NewService(micro.Registry(reg))
+	//调用微服务
 	micServ := micpro.NewMicproService("go.micro.srv.micpro",service.Client())
-	res,err := micServ.Call(context2.TODO(),nil)
+	//获取微服务结果
+	res,err := micServ.Call(context2.TODO(),&micpro.Request{})
 	if err != nil{
 		fmt.Println(err)
 	}
 
-	context.Writer.WriteString(res.Msg)
+	//处理微服务返回结果
+	context.JSON(http.StatusOK,gin.H{"code":11111,"data":res.Msg})
+
 }
 
 
